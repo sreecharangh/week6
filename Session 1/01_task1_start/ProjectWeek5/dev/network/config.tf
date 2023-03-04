8d6ff5314a228e22696a4e65a658d8e370bb700c
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 18:36:26 -0500

    made changes to dev/main.tf to point to the aws_network and globalvars and added proper escape sequence

--- Session 1/01_task1_start/ProjectWeek5/dev/network/config.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-acs730-week4-igeiman"      // Bucket where to SAVE Terraform State
+    bucket = "dev-acs730-week6-charan"      // Bucket where to SAVE Terraform State
     key    = "dev-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                     // Region where bucket is created
   }
