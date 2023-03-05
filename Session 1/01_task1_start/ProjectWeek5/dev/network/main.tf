9ecc10ec3311b58a1067c4d113f1e116cffb782c
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 19:17:40 -0500

    made changes to the source in main.tf

--- Session 1/01_task1_start/ProjectWeek5/dev/network/main.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/network/main.tf
@@ -3,7 +3,7 @@
 module "vpc-dev" {
   #source = "git::https://github.com/sreecharangh/week6/tree/main/Session%201/01_task1_start/modules/aws_network"
   #source = "git::https://github.com/sreecharangh/week6.git"
-  source = "local::/home/ec2-user/environment/week6/Session\\ 1/01_task1_start/modules/aws_network"
+  source = "./home/ec2-user/environment/week6/Session\\ 1/01_task1_start/modules/aws_network"
   #source              = "git@github.com:igeiman/aws_network.git"
   env                 = var.env
   vpc_cidr            = var.vpc_cidr
@@ -16,7 +16,7 @@ module "vpc-dev" {
 module "globalvars"{
   #source = "git::https://github.com/sreecharangh/week6/tree/main/Session%201/01_task1_start/modules/globalvars"
   #source = "git::https://github.com/sreecharangh/week6.git"
-  source = "local::/home/ec2-user/environment/week6/Session\\ 1/01_task1_start/modules/globalvars"
+  source = "./home/ec2-user/environment/week6/Session\\ 1/01_task1_start/modules/globalvars"
 }
 
 # Define tags locally
