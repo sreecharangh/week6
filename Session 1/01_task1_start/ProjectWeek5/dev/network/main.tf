26c3cae823a2f07295954cf0f5aac947a5583db3
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 20:34:53 -0500

    changed the main.tf

--- Session 1/01_task1_start/ProjectWeek5/dev/network/main.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/network/main.tf
@@ -7,7 +7,7 @@ module "vpc-dev" {
   #source              = "git@github.com:igeiman/aws_network.git"
   env                 = var.env
   vpc_cidr            = var.vpc_cidr
-  private_cidr_blocks = var.public_subnet_cidrs
+  private_cidr_block = var.public_subnet_cidrs
   prefix              = var.prefix
   default_tags        = var.default_tags
 }
