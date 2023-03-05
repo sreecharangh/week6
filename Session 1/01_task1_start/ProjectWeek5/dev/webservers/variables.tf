6fa6566df9279e3fc9e9cdb04b392973df94d7ca
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 23:29:34 -0500

    made changes to the dev/webservers

--- Session 1/01_task1_start/ProjectWeek5/dev/webservers/variables.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/webservers/variables.tf
@@ -17,6 +17,20 @@ variable "env" {
   description = "Deployment Environment"
 }
 
+variable "my_private_ip" {
+  type        = string
+  default     = "172.31.67.121"
+  description = "Private IP of my Cloud 9 station to be opened in bastion ingress"
+}
+
+# curl http://169.254.169.254/latest/meta-data/public-ipv4
+variable "my_public_ip" {
+  type        = string
+  default     = "44.200.30.237"
+  description = "Public IP of my Cloud 9 station to be opened in bastion ingress"
+}
+
+
 
 
 
