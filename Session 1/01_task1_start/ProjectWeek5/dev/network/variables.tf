b337a69bc2e571fc2e380b23c6b48cd7945b61d2
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 20:32:16 -0500

    changed the public subnet CIDRs values

--- Session 1/01_task1_start/ProjectWeek5/dev/network/variables.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/network/variables.tf
@@ -17,7 +17,7 @@ variable "prefix" {
 
 # Provision public subnets in custom VPC
 variable "public_subnet_cidrs" {
-  default     = ["10.1.0.0/24", "10.1.1.0/24"]
+  default     = ["10.1.0.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
