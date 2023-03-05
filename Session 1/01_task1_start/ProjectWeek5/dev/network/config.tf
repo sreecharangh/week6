6968f341f013715d2d30d5ec719f67cabd3a5565
sreecharangh <charanreddye.358@gmail.com>
2023-03-04 21:22:42 -0500

    added aws provider

--- Session 1/01_task1_start/ProjectWeek5/dev/network/config.tf
+++ Session 1/01_task1_start/ProjectWeek5/dev/network/config.tf
@@ -1,4 +1,8 @@
 terraform {
+  provider "aws" {
+region = "us-east-1"
+profile = "voclabs/user2346899=Sree_Charan_Erupaka"
+}
   backend "s3" {
     bucket = "dev-acs730-week6-sreecharangh"      // Bucket where to SAVE Terraform State
     key    = "dev-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
